fbf7409d3652023949cc8d70400d0bb00a32289b
jbaker8935 <farmerjohn1958@gmail.com>
2025-11-26 20:29:04 -0500

    fix: Correct build script path to reference the switcheroo directory

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 # Build wrapper for F256 Switcharoo
 # Run the actual build
 echo "Running full build..."
-../llvm-mos/f256dev/f256build.sh ../f256_switch "$@"
+../llvm-mos/f256dev/f256build.sh ../switcheroo "$@"
